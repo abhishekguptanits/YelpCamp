bb476327389ce12a1f6eabc48abe0f3ed2aface6
Abhishek <abhishekguptanits@gmail.com>
2023-02-28 23:00:03 +0530

    update footer content

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
